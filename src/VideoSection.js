4dc3223d5a30ef21d555c604f0e9025e9b75ca2e
ubuntu030 <ubuntu030@gmail.com>
2021-01-28 22:53:00 +0800

    樣式調整

--- src/VideoSection.js
+++ src/VideoSection.js
@@ -10,7 +10,7 @@ function VideoSection() {
 			<section className="history-section">
 				<div className="section-decs">search history</div>
 				<ul>
-					<li>1</li>
+					<li>喬瑟與虎與魚群『主題曲』 Eve - 蒼のワルツ (蒼之華爾茲)【中日歌詞】</li>
 					<li>2</li>
 					<li>3</li>
 				</ul>
