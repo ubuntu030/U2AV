e107a71e77e8d9ed546d7686081b3bc459d0857e
ubuntu030 <ubuntu030@gmail.com>
2021-02-01 12:01:06 +0800

    將元件移至components folder

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import { render } from "react-dom";
 
-import Header from './Header';
-import VideoSection from './VideoSection';
+import Header from './components/Header';
+import VideoSection from './components/VideoSection';
 
 import "./style.scss";
 
