8c3feedbb928ebc3000a24d8633eef30ddda5def
ubuntu030 <ubuntu030@gmail.com>
2021-02-02 15:54:40 +0800

    - fetching fake data while click "search button"

--- src/components/VideoSection.js
+++ src/components/VideoSection.js
@@ -1,6 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+import { fetchVideoPadding, fetchVideoSuccess, fetchVideoError } from "../actions";
 
 function VideoSection() {
+	const dispatch = useDispatch();
+	// 取得影片資訊
+	const fetchVideoInfo = async (url) => {
+		dispatch(fetchVideoPadding());
+		try {
+			const result = await (await fetch('./src/public/video_info.json')).json();
+			dispatch(fetchVideoSuccess);
+			console.log('[fetchVideoInfo] ok:', result);
+			return result;
+		} catch (error) {
+			console.log('[fetchVideoInfo] err:', error);
+			dispatch(fetchVideoError(error));
+		}
+	}
+
+	const handleSearchClick = () => {
+		fetchVideoInfo();
+	}
 
 	return (
 		<main className="video-container">
@@ -12,7 +33,7 @@ function VideoSection() {
 					<div className="video-download-container">
 						<input type="text" className="download-input" placeholder="Video URL" />
 						<div className="btn-group">
-							<button className="btn-primary">Search</button>
+							<button onClick={handleSearchClick} className="btn-primary">Search</button>
 							<button className="btn-primary">Download</button>
 						</div>
 					</div>
