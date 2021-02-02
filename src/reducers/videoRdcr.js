b963abaf3d87b65cd141a636fe2cc5a53037a78b
ubuntu030 <ubuntu030@gmail.com>
2021-02-02 21:10:19 +0800

    update

--- src/reducers/videoRdcr.js
+++ src/reducers/videoRdcr.js
@@ -5,11 +5,7 @@ import {
 } from "../actions";
 
 const initialState = {
-	"search_history": [{
-		"id": "xxx",
-		"name": "name",
-		"embedUrl": "url"
-	}],
+	"search_history": [],
 	"download_list": [{
 		"id": {
 			"id": "xxx",
@@ -19,14 +15,17 @@ const initialState = {
 		}
 	}]
 }
-
+// store updated but components https://stackoverflow.com/questions/58850699/useselector-not-updating-when-store-has-changed-in-reducer-reactjs-redux
 const videoReducer = (state = initialState, action) => {
 	switch (action.type) {
 		case FETCH_VIDEO_PADDING:
 			return state;
-		case 'FETCH_VIDEO_SUCCESS':
-			return state;
-		case 'FETCH_VIDEO_ERROR':
+		case FETCH_VIDEO_SUCCESS:
+			const { author, embed, title, videoId: id } = action.payload.videoDetails;
+			let search_history_list = [...state.search_history, { author, embed, title, id }]
+			let newState = Object.assign(state, { search_history: search_history_list });
+			return { ...newState };
+		case FETCH_VIDEO_ERROR:
 			return state;
 		default:
 			return state;
