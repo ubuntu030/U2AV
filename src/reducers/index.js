d6570bab2effc4bf12cda3bba44349bf38711263
ubuntu030 <ubuntu030@gmail.com>
2021-02-02 21:11:45 +0800

    combineReducers need to mutiple reducer to keep function working

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import videoReducer from "./videoRdcr";
+import audioReducer from "./audioRdcr";
 
+// NOTICE: combineReducers have to use multiple reducer,otherwise it can't do initial state when I tyr to useSelector in component first time.
 const rootReducer = combineReducers({
-	videoReducer
+	videoReducer,
+	audioReducer
 });
-
 export default rootReducer;
