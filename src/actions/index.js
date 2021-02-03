672cfb682fc753171e1952214fb72c957acf9e98
ubuntu030 <ubuntu030@gmail.com>
2021-02-03 10:03:11 +0800

    add action

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,6 +4,9 @@
 export const FETCH_VIDEO_PADDING = 'FETCH_VIDEO_PADDING';
 export const FETCH_VIDEO_SUCCESS = 'FETCH_VIDEO_SUCCESS';
 export const FETCH_VIDEO_ERROR = 'FETCH_VIDEO_ERROR';
+export const DOWNLOAD_VIDEO_PADDING = 'DOWNLOAD_VIDEO_PADDING';
+export const DOWNLOAD_VIDEO_SUCCESS = 'DOWNLOAD_VIDEO_SUCCESS';
+export const DOWNLOAD_VIDEO_ERROR = 'DOWNLOAD_VIDEO_ERROR';
 
 /**
  * VIDEO ACTION
@@ -13,17 +16,33 @@ export const fetchVideoPadding = () => {
 		type: FETCH_VIDEO_PADDING
 	}
 }
-
 export const fetchVideoSuccess = info => {
 	return {
 		type: FETCH_VIDEO_SUCCESS,
 		payload: info
 	}
 }
-
 export const fetchVideoError = error => {
 	return {
 		type: FETCH_VIDEO_ERROR,
+		payload: error
+	}
+}
+
+export const downloadVideoPadding = () => {
+	return {
+		type: DOWNLOAD_VIDEO_PADDING,
+	}
+}
+export const downloadVideoSuccess = info => {
+	return {
+		type: DOWNLOAD_VIDEO_SUCCESS,
+		payload: info
+	}
+}
+export const downloadVideoError = error => {
+	return {
+		type: DOWNLOAD_VIDEO_ERROR,
 		payload: error
 	}
 }
