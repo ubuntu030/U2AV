04eb4288b98d3b4e203a7e62ced6a418f14d1507
ubuntu030 <ubuntu030@gmail.com>
2021-02-03 11:03:40 +0800

    update isloading state to reducer

--- src/reducers/videoRdcr.js
+++ src/reducers/videoRdcr.js
@@ -9,30 +9,32 @@ import {
 
 const initialState = {
 	"search_history": [],
-	"download_list": []
+	"download_list": [],
+	"iframe_loading": false,
+	"download_loading": false
 }
 // store updated but components https://stackoverflow.com/questions/58850699/useselector-not-updating-when-store-has-changed-in-reducer-reactjs-redux
 const videoReducer = (state = initialState, action) => {
 	switch (action.type) {
 		case FETCH_VIDEO_PADDING:
-			return state;
+			return { ...state, iframe_loading: true };
 		case FETCH_VIDEO_SUCCESS:
 			var { author, embed, title, videoId: id } = action.payload.videoDetails;
 			let search_history_list = [...state.search_history, { author, embed, title, id }]
 			let newState = Object.assign(state, { search_history: search_history_list });
 			// TODO: prevent write same data in list
-			return { ...newState };
+			return { ...newState, iframe_loading: false };
 		case FETCH_VIDEO_ERROR:
-			return state;
+			return { ...state, iframe_loading: false };
 		case DOWNLOAD_VIDEO_PADDING:
-			return state;
+			return { ...state, download_loading: true };
 		case DOWNLOAD_VIDEO_SUCCESS:
 			var { author, embed, title, videoId: id } = action.payload.videoDetails;
 			// TODO: prevent write same data in list
-			newState = { ...state, download_list: [...state.download_list, { author, embed, title, id }] }
+			newState = { ...state, download_list: [...state.download_list, { author, embed, title, id }], download_loading: false }
 			return newState;
 		case DOWNLOAD_VIDEO_ERROR:
-			return state;
+			return { ...state, download_loading: false };;
 		default:
 			return state;
 	}
