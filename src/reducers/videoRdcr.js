d81ed3f94a5aa44d9ca6d4baf3547bf0cafa2212
ubuntu030 <ubuntu030@gmail.com>
2021-02-03 12:48:22 +0800

    download 資料避免重複寫入state search_history 將搜尋過的資料更新到最近一筆

--- src/reducers/videoRdcr.js
+++ src/reducers/videoRdcr.js
@@ -16,23 +16,26 @@ const initialState = {
 // store updated but components https://stackoverflow.com/questions/58850699/useselector-not-updating-when-store-has-changed-in-reducer-reactjs-redux
 const videoReducer = (state = initialState, action) => {
 	switch (action.type) {
+		// ---Process GET_INFO state below
 		case FETCH_VIDEO_PADDING:
 			return { ...state, iframe_loading: true };
 		case FETCH_VIDEO_SUCCESS:
 			var { author, embed, title, videoId: id } = action.payload.videoDetails;
-			let search_history_list = [...state.search_history, { author, embed, title, id }]
-			let newState = Object.assign(state, { search_history: search_history_list });
-			// TODO: prevent write same data in list
-			return { ...newState, iframe_loading: false };
+			// 若為重複的搜尋項目則將該筆紀錄放在更新最近歷史紀錄
+			var filtedResult = state.search_history.filter(item => item.id !== id);
+			return { ...state, search_history: [...filtedResult, { author, embed, title, id }], iframe_loading: false };
 		case FETCH_VIDEO_ERROR:
 			return { ...state, iframe_loading: false };
+		// ---Process DOWNLOAD state below
 		case DOWNLOAD_VIDEO_PADDING:
 			return { ...state, download_loading: true };
 		case DOWNLOAD_VIDEO_SUCCESS:
 			var { author, embed, title, videoId: id } = action.payload.videoDetails;
-			// TODO: prevent write same data in list
-			newState = { ...state, download_list: [...state.download_list, { author, embed, title, id }], download_loading: false }
-			return newState;
+			var filtedResult = state.download_list.filter(item => item.id === id);
+			if (filtedResult.length > 0) {
+				return state
+			}
+			return { ...state, download_list: [...state.download_list, { author, embed, title, id }], download_loading: false };
 		case DOWNLOAD_VIDEO_ERROR:
 			return { ...state, download_loading: false };;
 		default:
