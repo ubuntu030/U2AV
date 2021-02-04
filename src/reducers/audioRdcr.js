55db4b02c794334960f64cdd75936bec854525dd
ubuntu030 <ubuntu030@gmail.com>
2021-02-04 14:43:21 +0800

    reducer deal with loading state

--- src/reducers/audioRdcr.js
+++ src/reducers/audioRdcr.js
@@ -1,17 +1,18 @@
 import { FETCH_AUDIOS_PADDING, FETCH_AUDIOS_SUCCESS, FETCH_AUDIOS_ERROR } from '../actions';
 
 const initialState = {
-	audio_list: []
+	audio_list: [],
+	list_loading: false
 };
 
 const audioReducer = (state = initialState, action) => {
 	switch (action.type) {
 		case FETCH_AUDIOS_PADDING:
-			return state;
+			return { ...state, list_loading: true };
 		case FETCH_AUDIOS_SUCCESS:
-			return { ...state, audio_list: [...action.payload] };
+			return { ...state, audio_list: [...action.payload], list_loading: false };
 		case FETCH_AUDIOS_ERROR:
-			return state;
+			return { ...state, list_loading: false };
 		default:
 			return state;
 	}
