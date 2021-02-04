407f4d6b5ad2c5209f66fc0e0af5f6cda2de4e0c
ubuntu030 <ubuntu030@gmail.com>
2021-02-04 14:20:40 +0800

    reducer about Audio list

--- src/reducers/audioRdcr.js
+++ src/reducers/audioRdcr.js
@@ -1,8 +1,17 @@
+import { FETCH_AUDIOS_PADDING, FETCH_AUDIOS_SUCCESS, FETCH_AUDIOS_ERROR } from '../actions';
 
-const initialState = {};
+const initialState = {
+	audio_list: []
+};
 
 const audioReducer = (state = initialState, action) => {
 	switch (action.type) {
+		case FETCH_AUDIOS_PADDING:
+			return state;
+		case FETCH_AUDIOS_SUCCESS:
+			return { ...state, audio_list: [...action.payload] };
+		case FETCH_AUDIOS_ERROR:
+			return state;
 		default:
 			return state;
 	}
