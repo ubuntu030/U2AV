6621802cb85c0e3f221e0cb03fde8bb9bc5ad488
ubuntu030 <ubuntu030@gmail.com>
2021-02-04 11:09:32 +0800

    action of Convert

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,10 +7,14 @@ export const FETCH_VIDEO_ERROR = 'FETCH_VIDEO_ERROR';
 export const DOWNLOAD_VIDEO_PADDING = 'DOWNLOAD_VIDEO_PADDING';
 export const DOWNLOAD_VIDEO_SUCCESS = 'DOWNLOAD_VIDEO_SUCCESS';
 export const DOWNLOAD_VIDEO_ERROR = 'DOWNLOAD_VIDEO_ERROR';
+export const CONVERT_VIDEO_PADDING = 'CONVERT_VIDEO_PADDING';
+export const CONVERT_VIDEO_SUCCESS = 'CONVERT_VIDEO_SUCCESS';
+export const CONVERT_VIDEO_ERROR = 'CONVERT_VIDEO_ERROR';
 
 /**
  * VIDEO ACTION
  */
+// GET VIDEO INFO
 export const fetchVideoPadding = () => {
 	return {
 		type: FETCH_VIDEO_PADDING
@@ -28,7 +32,7 @@ export const fetchVideoError = error => {
 		payload: error
 	}
 }
-
+// DOWNLOAD VIDEO
 export const downloadVideoPadding = () => {
 	return {
 		type: DOWNLOAD_VIDEO_PADDING,
@@ -44,5 +48,24 @@ export const downloadVideoError = error => {
 	return {
 		type: DOWNLOAD_VIDEO_ERROR,
 		payload: error
+	}
+}
+// CONVERT VIDEO
+export const convertVideoPadding = (id) => {
+	return {
+		type: CONVERT_VIDEO_PADDING,
+		payload: id
+	}
+}
+export const convertVideoSuccess = (info) => {
+	return {
+		type: CONVERT_VIDEO_SUCCESS,
+		payload: info
+	}
+}
+export const convertVideoError = ({ id, error }) => {
+	return {
+		type: CONVERT_VIDEO_ERROR,
+		payload: { id, error }
 	}
 }
