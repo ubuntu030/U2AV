f6fa35fcee8d4b437193bc6ae9ba04bf6119c086
ubuntu030 <ubuntu030@gmail.com>
2021-02-04 12:57:39 +0800

    use grid to split template

--- src/components/AudioSection.js
+++ src/components/AudioSection.js
@@ -4,24 +4,19 @@ import { useSelector } from "react-redux";
 function AudioSection() {
 	return (
 		<main className="audio-container">
-			<section className="audio-section-1">
-				<div className="play-section">
-					Player
-				</div>
-				<div className="editor-section">
-					Editor
-				</div>
+			<section className="audio-player">
+				audio-player
+			</section>
+			<section className="audio-editor">
+				audio-editor
 			</section>
 			<section className="audio-list">
-				<div className="section-decs">Audio List</div>
-				<ul>
-					<li>喬瑟與虎與魚群『主題曲』 Eve - 蒼のワルツ (蒼之華爾茲)【中日歌詞】</li>
-					<li>喬瑟與虎與魚群『主題曲』 Eve - 蒼のワルツ (蒼之華爾茲)【中日歌詞】</li>
-					<li>喬瑟與虎與魚群『主題曲』 Eve - 蒼のワルツ (蒼之華爾茲)【中日歌詞】</li>
-					<li>喬瑟與虎與魚群『主題曲』 Eve - 蒼のワルツ (蒼之華爾茲)【中日歌詞】</li>
-				</ul>
+				audio-list
 			</section>
 		</main>
+
+
+
 	)
 }
 
