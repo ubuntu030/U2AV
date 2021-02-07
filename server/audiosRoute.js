2976249c10f57ee8b9364188bad58a33bcbf24c6
ubuntu030 <ubuntu030@gmail.com>
2021-02-07 17:52:34 +0800

    response data including meta data

--- server/audiosRoute.js
+++ server/audiosRoute.js
@@ -1,7 +1,9 @@
 const fs = require('fs').promises;
+const util = require('util');
 const path = require('path');
 const router = require('express').Router();
 const ffmpeg = require('fluent-ffmpeg');
+const ffprobePms = util.promisify(ffmpeg.ffprobe);
 
 // const PATH_VIDEO_FOLDER = path.join(__dirname, '../src/media/Video');
 const PATH_AUDIO_FOLDER = path.join(__dirname, '../src/media/Audio');
@@ -12,7 +14,11 @@ router.get('/', async (req, res) => {
 	try {
 		const result = await fs.readdir(PATH_AUDIO_FOLDER);
 		console.log('[audiosRoute] ok:', result);
-		res.send(result);
+		const listWithMeta = await Promise.all(result.map(async title => {
+			return { title, meta: await ffprobePms(path.join(PATH_AUDIO_FOLDER, title)) };
+		}))
+
+		res.send(listWithMeta);
 	} catch (error) {
 		console.error('[audiosRoute] err:', error);
 		res.send(error);
