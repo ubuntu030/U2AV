177c25fbf97d4e9280f9b82aeac8ae886bab7488
ubuntu030 <ubuntu030@gmail.com>
2021-02-15 18:14:01 +0800

    -check the value from req.body

--- server/editorRoute.js
+++ server/editorRoute.js
@@ -9,19 +9,25 @@ ffmpeg.setFfmpegPath(PATH_FFMEPG);
 
 router.use(function (req, res, next) {
 	const { title, bg, ed } = req.body;
-	if (title && ed > bg) {
-		next();
-	} else {
+	if (!title || typeof title !== 'string') {
 		res.send({
-			error: 'empty param',
+			error: 'wrong title',
 		});
+		return;
 	}
+	if (ed === 0 || bg >= ed) {
+		res.send({
+			error: 'wrong time',
+		});
+		return;
+	}
+	next();
 });
 
 router.post('/', (req, res) => {
-	const { title, bg, ed } = req.body;
-	const filePath = path.join(PATH_AUDIO_FOLDER, 'test.wav');
-	const cnvtFilePath = path.join(PATH_AUDIO_FOLDER, title);
+	const { orgTitle, title, bg, ed } = req.body;
+	const filePath = path.join(PATH_AUDIO_FOLDER, orgTitle);
+	const cnvtFilePath = path.join(PATH_AUDIO_FOLDER, title +'.wav');
 	ffmpeg(filePath)
 		.setStartTime(bg)
 		.setDuration(ed - bg)
@@ -30,14 +36,21 @@ router.post('/', (req, res) => {
 			if (!err) {
 				console.log('conversion Done')
 				res.send({
-					data: 'qoo'
+					title: title,
+					audioPath: cnvtFilePath
+				});
+			} else {
+				res.send({
+					error: err
 				});
 			}
 		})
 		.on('error', function (error) {
 			console.log('error: ', error)
-			res.status(500).end();
-		}).run()
+			res.send({
+				error: error.message
+			});
+		}).run();
 });
 
 module.exports = router;
